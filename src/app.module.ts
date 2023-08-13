b4336fc0ad1ad2b7f8c3c340c8ff5e88c4fdce6d
Евгений Швецов <67862313+EvgenyShvetcov@users.noreply.github.com>
2023-08-14 01:58:53 +0300

    SomeErrorMongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TrackModule } from './track/track.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
-@Module({ controllers: [AppController], providers: [AppService] })
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/MusicPlatform'),
+    TrackModule,
+  ],
+})
 export class AppModule {}
