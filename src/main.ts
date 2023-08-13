1f9b8c571e82abc1b04f3fa846ecc9c85744f8f5
Евгений Швецов <67862313+EvgenyShvetcov@users.noreply.github.com>
2023-08-13 23:27:22 +0300

    firstCommit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+const start = async () => {
+  try {
+    console.log('asd');
+
+    const PORT = 8000;
+    const app = await NestFactory.create(AppModule);
+    await app.listen(PORT, () => console.log(`server started on PORT ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
